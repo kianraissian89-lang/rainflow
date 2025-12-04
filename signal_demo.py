cc84b79e118b448f6979cd8b02efe915d7d8aa7a
kianraissian89-lang <kianraissian89@gmail.com>
2025-12-04 09:27:44 -0800

    return tuples of reversal points and timestamps

--- signal_demo.py
+++ signal_demo.py
@@ -9,7 +9,13 @@ time = np.linspace(0, 4.0, 4 * sample_rate)
 signal = [0.2 + 0.5 * sin(t) + 0.2 * cos(10*t) + 0.2 * sin(4*t) for t in time]
 
 cycles = rainflow.count_cycles(signal)
-print(cycles)
+#print(cycles)
+
+from src.rainflow import reversals
+
+reversal_points = list(reversals(signal))
+reversal_with_time = [(float(time[index]), float(magnitude)) for index, magnitude in reversal_points]
+print(reversal_with_time)
 
 
 plt.figure(figsize=(10, 4))
