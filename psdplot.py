9a337e242fefc1ce06c49e2a73920c12616c297b
kianraissian89-lang <kianraissian89@gmail.com>
2025-12-11 18:42:55 -0800

    ability to compare the raw and already processed psd datasets

--- psdplot.py
+++ psdplot.py
@@ -6,15 +6,25 @@ import scipy.signal
 from scipy.signal import welch
 
 
-# Load CSV data
+# Load CSV data from the raw csv file
 acceldata = pd.read_csv('RAWDATA - Waveform.csv', usecols=['FrontAxle_Left Front (Max) (G)'])
 data = acceldata.values.flatten()  # Convert to 1D array
-nfreq = 101 # number of frequency points
-sample_rate = 2000  # samples per second (adjust as needed)
+sample_rate = 2000  # samples per second 
 
-(f, S)= scipy.signal.welch(data, sample_rate, nperseg=len(data)/nfreq)
+# define the number frequencies to split the psd data into, then run the psd function
+nfreq = 102
+nperseg = nfreq * 2
+(f, S)= scipy.signal.welch(data, sample_rate, nperseg=nperseg)
 
-plt.loglog(f, S)
+#get the already processed psd data, flatten it into arrays
+psd = pd.read_csv('PSD - Power Spectral Density_Front_Axle.csv', usecols=['FrontAxle_Left Front (G²/Hz)']).values.flatten()
+freqs = pd.read_csv('PSD - Power Spectral Density_Front_Axle.csv', usecols=['Frequency (Hz)']).values.flatten()
+
+#plot the psd data
+print(len(freqs)), print(len(f))
+plt.loglog(f, S, label='Python Manipulated PSD')
+plt.loglog(freqs, psd, label='Already Processed PSD')
+plt.legend()
 plt.xlim([0, 1000])
 plt.xlabel('frequency [Hz]')
 plt.ylabel('PSD [G^2/Hz]')
